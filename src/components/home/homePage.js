f1a2e79de0fd0ff71e64fb55d77ea23d97ea2273
kritydangol <kritydangol1@gmail.com>
2024-02-28 15:38:37 +0545

    home page tagline

--- src/components/home/homePage.js
+++ src/components/home/homePage.js
@@ -1,8 +1,13 @@
 import React from "react";
 import bgImg from "../../assets/images/home.png";
 import { Link } from "react-router-dom";
+import { API_URL } from "../../constants";
+import useFetch from "../../utils/useFetch";
 
 const HomePage = () => {
+  // fetching data
+  const { data } = useFetch(`${API_URL}home-page-data/`);
+
   return (
     <div
       className="snap-always snap-start bg-fixed bg-center bg-cover min-h-screen z-[-2]"
@@ -13,13 +18,16 @@ const HomePage = () => {
           <h1 className="md:text-7xl text-4xl text-white font-bold mb-4">
             Welcome to Chariot
           </h1>
-          <p className="md:text-4xl text-lg text-white font-light  pb-4">
-            Where dream meets destinations
-          </p>
+          {data && (
+            <p className="md:text-4xl text-lg text-white font-light  pb-4">
+              {data[1].description}
+            </p>
+          )}
+
           <Link to="/services">
-          <button className="md:text-lg text-md font-medium text-white py-2 px-4 border rounded hover:bg-red-900 ">
-            Our Services
-          </button>
+            <button className="md:text-lg text-md font-medium text-white py-2 px-4 border rounded hover:bg-red-900 ">
+              Our Services
+            </button>
           </Link>
         </div>
       </div>

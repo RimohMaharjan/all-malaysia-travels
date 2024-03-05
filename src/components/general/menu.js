fd5ad334971eb4b239aa92183adf41271620704d
RimohMaharjan <rimohmaharjan1@gmail.com>
2024-03-05 16:11:50 +0545

    nav menu updated

--- src/components/general/menu.js
+++ src/components/general/menu.js
@@ -1,16 +1,8 @@
 import React, { useContext } from "react";
 import { MenuContext } from "react-flexible-sliding-menu";
 import { RxCross1 } from "react-icons/rx";
-import { FaPhoneAlt } from "react-icons/fa";
-import { IoLocationSharp } from "react-icons/io5";
-import { IoIosMail } from "react-icons/io";
-import { API_URL } from "../../constants";
-import useFetch from "../../utils/useFetch";
 
 function Menu() {
-  const { data: footer, error: footer_error } = useFetch(
-    `${API_URL}home-page-data/`
-  );
 
   const { closeMenu } = useContext(MenuContext);
   const pathname = window.location.pathname;
@@ -87,6 +79,10 @@ function Menu() {
       scroll: scrollToRep,
     },
     {
+      label: "Services",
+      path: "/services",
+    },
+    {
       label: "Promotions",
       path: "/",
       scroll: scrollToBanner,
@@ -117,68 +113,28 @@ function Menu() {
       </div>
 
       <div className="flex flex-col space-y-2 w-[80vw] mx-auto text-4xl pt-6">
-        {pathname === "/" ? (
-          <>
-            {navItems.map((item, index) => (
+        {navItems.map((item, index) => (
+          <div key={index}>
+            {pathname === "/" && item.scroll ? (
               <p
-                key={index}
                 onClick={item.scroll}
                 className="cursor-pointer text-[30px] pb-5 "
               >
                 {item.label}
               </p>
-            ))}
-          </>
-        ) : (
-          <>
-            {navItems.map((item, index) => (
+            ) : (
               <a
-                key={index}
                 href={item.path}
                 className="cursor-pointer text-[30px] pb-5 block"
               >
                 {item.label}
               </a>
-            ))}
-          </>
-        )}
-        <a href="/services" className="cursor-pointer text-[30px]">
-          Services
-        </a>
+            )}
+          </div>
+        ))}
       </div>
 
-      {footer ? (
-        <div className="flex flex-col space-y-2 w-[80vw] mx-auto text-md pt-20">
-          {footer && (
-            <>
-              <a className={` text-md`} href={`tel: ${footer[2].description}`}>
-                <FaPhoneAlt className="inline mr-2" />
-                {footer[2].description}
-              </a>
-              <a
-                className={`text-md `}
-                href={footer[5].description}
-                target="_blank"
-                rel="noreferrer"
-              >
-                <IoLocationSharp className="inline mr-2" />
-                {footer[4].description}
-              </a>
-              <a
-                className={`text-md`}
-                href={`mailto: ${footer[3].description}`}
-              >
-                <IoIosMail className="inline mr-2" />
-                {footer[3].description}
-              </a>
-            </>
-          )}
-        </div>
-      ) : footer_error ? (
-        <p className="text-center">{footer_error}</p>
-      ) : (
-        <p className="text-center">loading</p>
-      )}
+      
     </div>
   );
 }

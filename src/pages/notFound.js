ead0c57920f05733afb346155d501e0c72f3b4a0
kritydangol <kritydangol1@gmail.com>
2024-03-06 16:49:44 +0545

    404 updated

--- src/pages/notFound.js
+++ src/pages/notFound.js
@@ -10,7 +10,7 @@ const NotFound = () => {
           alt="404"
         />
         <h1 className="text-4xl pb-5">Page Not Found.</h1>
-        <p className="pb-5">That page doesn't seem to exist.</p>
+        <p className="pb-5 text-center">That page doesn't seem to exist.</p>
         <a href="/" className="button-primary font-bold">
           Go Home ➔
         </a>

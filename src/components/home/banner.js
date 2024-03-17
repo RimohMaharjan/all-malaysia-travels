2c112771f66312f745ba79fb82d4da25198a40ef
kritydangol <kritydangol1@gmail.com>
2024-03-17 16:38:38 +0545

    🤏🔃 banner

--- src/components/home/banner.js
+++ src/components/home/banner.js
@@ -34,7 +34,6 @@ const Banner = ({ promotionRef }) => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    speed: 2000,
     autoplaySpeed: 4000,
     cssEase: "linear",
     nextArrow: <NextArrow />,
